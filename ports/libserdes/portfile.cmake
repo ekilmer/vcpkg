5642c2078163f256e1450e65078578e0c6351993
Eric Kilmer <eric.d.kilmer@gmail.com>
2020-08-27 14:29:22 -0400

    Use ekilmer libserdes fork

--- ports/libserdes/portfile.cmake
+++ ports/libserdes/portfile.cmake
@@ -1,37 +1,18 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO confluentinc/libserdes
-    REF a50fed317403fdef64b95c061614a5148597f401 # v6.1.0-betaxxxxxx (current HEAD master)
-    SHA512 d19e924a57f56a63f926f668704f903b4867c150add61f57a5657accbe8a60aad6d3632f7ed571d41049f3ab34d0e1e3228916c322a8678413f363ace2c9fd23
-    HEAD_REF master
-    PATCHES
-          support-latest-avro.patch
+    REPO ekilmer/libserdes
+    # fork of (current HEAD master) with cmake and latest avro support
+    REF f257574b256cbbdd12a2a6d7862379ebc2193b83
+    SHA512 253ea72d7efb43cce0bc8a462933716c01149b26ab9f9e7ecfef5be5c1aaf81eb7e61586bf10faf6fdffb9b78fee2a7a67949a2a4a8881186da26a31c61279c4
+    HEAD_REF dev
 )
 
-set(LDFLAGS "-L${CURRENT_INSTALLED_DIR}/lib")
-set(CFLAGS "-I${CURRENT_INSTALLED_DIR}/include")
-set(CPPFLAGS "-I${CURRENT_INSTALLED_DIR}/include")
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+)
 
-if (VCPKG_TARGET_IS_WINDOWS)
-else() # Build in UNIX
-    set(ENV{PKG_CONFIG_PATH} "${CURRENT_INSTALLED_DIR}/lib/pkgconfig")
-  vcpkg_execute_required_process(
-    COMMAND ./configure --prefix=${CURRENT_PACKAGES_DIR} "--CFLAGS=${CFLAGS}" "--CPPFLAGS=${CPPFLAGS}" "--LDFLAGS=${LDFLAGS}"
-    WORKING_DIRECTORY ${SOURCE_PATH}
-    LOGNAME configure-${TARGET_TRIPLET}
-  )
+vcpkg_install_cmake()
 
-  vcpkg_execute_required_process(
-    COMMAND make -j ${VCPKG_CONCURRENCY} libs
-    WORKING_DIRECTORY ${SOURCE_PATH}
-    LOGNAME build-${TARGET_TRIPLET}
-  )
-
-  vcpkg_execute_required_process(
-    COMMAND make install
-    WORKING_DIRECTORY ${SOURCE_PATH}
-    LOGNAME install-${TARGET_TRIPLET}
-  )
-endif()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 configure_file(${SOURCE_PATH}/LICENSE ${CURRENT_PACKAGES_DIR}/share/libserdes/copyright COPYONLY)
